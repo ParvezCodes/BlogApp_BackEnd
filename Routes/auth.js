384f72c246b3d1695c78344452359039f2207fd8
ParvezCodes <parvez0035@gamil.com>
2024-07-23 14:10:54 +0530

    updated

--- Routes/auth.js
+++ Routes/auth.js
@@ -68,7 +68,7 @@ router.post("/login", validateLogin, async (req, res) => {
       httpOnly: true,
       maxAge: 60 * 60 * 1000,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
     });
 
     return res.status(200).json({ msg: "Login Successfully", user });
