f02cb496230cbc2adce952fedb9d46b3595315f9
ParvezCodes <parvez0035@gamil.com>
2024-07-23 13:33:12 +0530

    updated

--- Middleware/authenticate.js
+++ Middleware/authenticate.js
@@ -6,6 +6,8 @@ export const authenticate = (req, res, next) => {
   // const token = req.headers.authorization?.split(" ")[1];
   const token = req.cookies.token;
 
+  console.log(token);
+
   if (!token) {
     return res.status(401).json({ msg: "Unauthorized User" });
   }
