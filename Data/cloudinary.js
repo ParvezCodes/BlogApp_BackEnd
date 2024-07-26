9271dc924dfd890a5cf6076c72fdd80e6e215bbd
ParvezCodes <parvez0035@gamil.com>
2024-07-26 10:55:37 +0530

    updated

--- Data/cloudinary.js
+++ Data/cloudinary.js
@@ -1,10 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
-dotenv.config(); // Adjust path as necessary
-
-console.log("env :", process.env.CLOUDINARY_API_KEY);
-console.log("env :", process.env.CLOUDINARY_API_SECRET);
-console.log("env :", process.env.CLOUDINARY_CLOUD_NAME);
+dotenv.config(); 
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
